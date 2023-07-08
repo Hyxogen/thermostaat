ad1845113873aec18f018d17debe251fc0283d02
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-07-08 09:57:33 +0200

    improve dialogue

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,12 +1,79 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Dialogue
-{
-    public string name;
+public interface IDialogueBase {}
 
-    [TextArea(3, 10)]
-    public string[] dialogue;
+public struct DialogueText : IDialogueBase {
+    public string actorName;
+    public string dialogue;
+
+    public DialogueText(string actorName, string dialogue) {
+        this.actorName = actorName;
+        this.dialogue = dialogue;
+    }
+}
+
+public interface IDialogueChoice : IDialogueBase {
+    string ADesc();
+    string BDesc();
+    void OptionA();
+    void OptionB();
+}
+
+public struct LambdaDialogueChoice : IDialogueChoice {
+    public Action optionA;
+    public string descriptionA;
+    public Action optionB;
+    public string descriptionB;
+
+    public LambdaDialogueChoice(string descriptionA, Action optionA, string descriptionB, Action optionB) {
+        this.descriptionA = descriptionA;
+        this.optionA = optionA;
+        this.descriptionB = descriptionB;
+        this.optionB = optionB;
+    }
+
+    public void OptionA() {
+        optionA.Invoke();
+    }
+
+    public string ADesc() {
+        return descriptionA;
+    }
+
+    public void OptionB() {
+        optionB.Invoke();
+    }
+
+    public string BDesc() {
+        return descriptionB;
+    }
+}
+
+public interface IDialogue {
+    public IEnumerator<IDialogueBase> Next(GameManager manager);
+}
+
+public class SimpleDialogue : IDialogue {
+    public IEnumerator<IDialogueBase> Next(GameManager manager) {
+        string name = "David";
+        string you = "You";
+
+        bool saves_cat = false;
+
+        yield return new DialogueText(name, "Hey there John");
+        yield return new DialogueText(name, "Have you seen my cat Whiskers?");
+        yield return new DialogueText(you, "Nope");
+        yield return new DialogueText(name, "Darn, was hoping you saw him");
+        yield return new DialogueText(name, "Could you catch him again if he's around?");
+
+        yield return new LambdaDialogueChoice("Yeah ofcource", () => saves_cat = true, "No, fuck your cat", () => saves_cat = false);
+
+        if (saves_cat) {
+            yield return new DialogueText(name, "Thanks man");
+        } else {
+            yield return new DialogueText(name, "Understandable, have a great day");
+        }
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,6 +6,7 @@ public class GameManager : MonoBehaviour
     public HeroInstance hero;
     public QuestListUI questList;
     public ItemInventoryUI heroInventory;
+    public List<QuestData> allQuests;
 
     private void Start()
     {
@@ -21,5 +23,17 @@ public class GameManager : MonoBehaviour
         {
             Debug.LogError("NOT POGGERS");
         }
+    }
+
+    public void StartNamedQuest(string name) {
+        Debug.Log("Imagine that the quest " + name + " started");
+        Debug.Log(hero.heroData.heroName);
+        QuestData quest = allQuests.Find(quest => quest.questName == name);
+        if (quest == null) {
+            Debug.Log("No quest found with the name: " + name);
+        } else {
+            //questList.quests.Add(quest);
+        }
+        //questList.quests.Find(quest => quest.questData.questName == name);
     }
 }
